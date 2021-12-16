c96ea29c134394bc034f038aaeace089dfa2320c
croyli <pasnefedov@gmail.com>
2021-12-16 19:31:04 +0300

    goo

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from 'express'
 import path from 'path'
 import cors from 'cors'
 import sockjs from 'sockjs'
-// import { renderToStaticNodeStream } from 'react-dom/server'
-// import React from 'react'
+import { renderToStaticNodeStream } from 'react-dom/server'
+import React from 'react'
 import axios from 'axios'
 import cookieParser from 'cookie-parser'
 import config from './config'
@@ -13,17 +13,17 @@ const { readFile, writeFile, unlink } = require('fs').promises
 
 require('colors')
 
-// let Root
-// try {
-//   // eslint-disable-next-line import/no-unresolved
-//   Root = require('../dist/assets/js/ssr/root.bundle').default
-// } catch {
-//   console.log('SSR not found. Please run "yarn run build:ssr"'.red)
-// }
+let Root
+try {
+  // eslint-disable-next-line import/no-unresolved
+  Root = require('../dist/assets/js/ssr/root.bundle').default
+} catch {
+  console.log('SSR not found. Please run "yarn run build:ssr"'.red)
+}
 
 let connections = []
 
-const port = process.env.PORT || 230
+const port = process.env.PORT || 232
 const server = express()
 
 const setHeaders = (req, res, next) => {
@@ -174,30 +174,30 @@ server.get('/*', (req, res) => {
   )
 })
 
-// const [htmlStart, htmlEnd] = Html({
-//   body: 'separator',
-//   title: 'Skillcrucial'
-// }).split('separator')
+const [htmlStart, htmlEnd] = Html({
+  body: 'separator',
+  title: 'Skillcrucial'
+}).split('separator')
 
-// server.get('/', (req, res) => {
-//   const appStream = renderToStaticNodeStream(<Root location={req.url} context={{}} />)
-//   res.write(htmlStart)
-//   appStream.pipe(res, { end: false })
-//   appStream.on('end', () => {
-//     res.write(htmlEnd)
-//     res.end()
-//   })
-// })
+server.get('/', (req, res) => {
+  const appStream = renderToStaticNodeStream(<Root location={req.url} context={{}} />)
+  res.write(htmlStart)
+  appStream.pipe(res, { end: false })
+  appStream.on('end', () => {
+    res.write(htmlEnd)
+    res.end()
+  })
+})
 
-// server.get('/*', (req, res) => {
-//   const appStream = renderToStaticNodeStream(<Root location={req.url} context={{}} />)
-//   res.write(htmlStart)
-//   appStream.pipe(res, { end: false })
-//   appStream.on('end', () => {
-//     res.write(htmlEnd)
-//     res.end()
-//   })
-// })
+server.get('/*', (req, res) => {
+  const appStream = renderToStaticNodeStream(<Root location={req.url} context={{}} />)
+  res.write(htmlStart)
+  appStream.pipe(res, { end: false })
+  appStream.on('end', () => {
+    res.write(htmlEnd)
+    res.end()
+  })
+})
 
 const app = server.listen(port)
 
