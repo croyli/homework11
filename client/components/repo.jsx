4f1315ef38423b926e82cefb6d78873a649d9bf9
croyli <pasnefedov@gmail.com>
2022-10-20 22:52:06 +0300

    com

---
+++ client/components/repo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import Head from './head'
+
+
+
+const Repo = () => {
+const {userName} = useParams()
+return(
+  <div>
+    <Head title="Hello" />
+   {userName}
+  </div>
+)
+}
+
+Repo.propTypes = {}
+
+export default React.memo(Repo)
--- client/config/root.jsx
+++ client/config/root.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from './redux'
+import store from '../redux'
 
 const Root = () => {
-  return <Provider store={store}></Provider>
+  return <Provider store={store}>hello</Provider>
 }
-
 export default Root
